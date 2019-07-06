e10fe37e58f3ec801e7179ef3c2633198351bb51
Leandro Hanc <leandrohanc@gmail.com>
2019-07-06 12:22:19 -0300

    Agregar rutas para respuetas

--- routes.js
+++ routes.js
@@ -4,19 +4,27 @@ require('./models/Respuestas');
 
 const router = require('express').Router();
 
-const wrapAsync = require('./middleware/wrapAsync');
+const catchingAsyncErrors = require('./middleware/wrapAsync');
 
 const frases = require('./controllers/fraseController');
 const palabrasClave = require('./controllers/palabrasClaveController');
+const respuestas = require('./controllers/respuestasController');
 
 // FRASES
-router.get('/frases', wrapAsync(frases.todasLasFrases));
-router.get('/frases/buscar', wrapAsync(frases.buscarUnaFrase));
-router.post('/frases', wrapAsync(frases.agregarFrase));
-router.patch('/frases/:id', wrapAsync(frases.editarFrase));
-router.delete('/frases/:id', wrapAsync(frases.borrarFrase));
+router.get('/frases', catchingAsyncErrors(frases.todasLasFrases));
+router.get('/frases/buscar', catchingAsyncErrors(frases.buscarUnaFrase));
+router.post('/frases', catchingAsyncErrors(frases.agregarFrase));
+router.patch('/frases/:id', catchingAsyncErrors(frases.editarFrase));
+router.delete('/frases/:id', catchingAsyncErrors(frases.borrarFrase));
 
 // PALABRAS CLAVE
-router.get('/palabrasClave', wrapAsync(palabrasClave.procesarMensaje));
+router.get('/palabrasClave', catchingAsyncErrors(palabrasClave.procesarMensaje));
+
+// RESPUESTA
+router.get('/respuestas', catchingAsyncErrors(respuestas.todasLasRespuestas));
+router.get('/respuestas/buscar', catchingAsyncErrors(respuestas.buscarUnaRespuesta));
+router.post('/respuestas', catchingAsyncErrors(respuestas.agregarRespuesta));
+router.patch('/respuestas/:id', catchingAsyncErrors(respuestas.editarRespuesta));
+router.delete('/respuestas/:id', catchingAsyncErrors(respuestas.borrarRespuesta));
 
 module.exports = router;

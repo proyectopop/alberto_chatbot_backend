156d2a2830f6f3e6caa9476a9a7ba8bbe4dd84b4
Leandro Hanc <leandrohanc@gmail.com>
2019-07-14 15:26:52 -0300

    Usar variables de entorno

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+const keys = {
+  googleProjectID: process.env.GOOGLE_PROJECT_ID,
+  googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
+  googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY,
+  dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
+  dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_SESSION_LANGUAGE_CODE,
+};
+
+module.exports = keys;

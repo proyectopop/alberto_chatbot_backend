fc2813a8500f82d0604a1429012c2b1e5feba6d7
Leandro Hanc <leandrohanc@gmail.com>
2019-08-03 12:17:12 -0300

    Remover helper no utilizado

--- services/helpers/buildContextPath.js
+++
@@ -1,5 +0,0 @@
-const buildContextPath = (contextName = '') => `projects/${process.env.GOOGLE_PROJECT_ID}
-/agent/sessions/${process.env.DIALOGFLOW_SESSION_ID}
-  /contexts/${contextName}`;
-
-module.exports = buildContextPath;

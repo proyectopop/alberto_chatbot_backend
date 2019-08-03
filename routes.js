85ff6cc036d792103931fc79db93d710d47558ab
Leandro Hanc <leandrohanc@gmail.com>
2019-08-03 12:16:23 -0300

    Remover servicio que es utilizado en admin

--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const catchingAsyncErrors = require('./middleware/wrapAsync');
 
-const Imagenes = require('./services/images');
 const Alberto = require('./services/chatbot');
 
 // TEST
@@ -10,8 +9,5 @@ router.get('/alberto/prueba', (req, res) => res.status(200).json());
 // CHAT BOT
 router.post('/alberto/evento', catchingAsyncErrors(Alberto.enviaEvento));
 router.post('/alberto/mensaje', catchingAsyncErrors(Alberto.recibeMensaje));
-
-// IMAGENES
-router.post('/alberto/imagenes', Imagenes.multerUploads, Imagenes.processUpload);
 
 module.exports = router;
